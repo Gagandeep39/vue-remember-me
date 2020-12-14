a38b231ec2940668fd2159b5688ccb4d277382d3
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-14 09:18:59 +0000

    Created dialog compoent

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard';
 import BaseButton from './components/ui/BaseButton';
+import BaseDialog from './components/ui/BaseDialog';
 
 createApp(App)
   .component('base-card', BaseCard)
   .component('base-button', BaseButton)
+  .component('base-dialog', BaseDialog)
   .mount('#app');
