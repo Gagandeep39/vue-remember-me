d8b66d792a75fefdb88b8b17ea00f727e66f6286
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-14 08:05:13 +0000

    Created base button component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton';
 
 createApp(App)
   .component('base-card', BaseCard)
+  .component('base-button', BaseButton)
   .mount('#app');
